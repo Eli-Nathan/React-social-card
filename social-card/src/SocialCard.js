71869ecf17bec8228c7f0d1651ad99c3aa9cd7e4
Eli Nathan <ely.nathan93@gmail.com>
2018-12-25 23:19:51 +0000

    Add unsplash images and heart icon

---
+++ social-card/src/SocialCard.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import './SocialCard.css';
+import heart from './images/heart.svg';
+
+class SocialCard extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <div className="card__image">
+          <img
+            src={this.props.imagePath}
+            alt="Image alt text"
+          />
+        </div>
+        <div className="card__info">
+          <h2>{this.props.title}</h2>
+          <div className="interactions">
+            <button className="interactions__icon interactions__icon--heart">
+              <object
+                className="heartObject"
+                type="image/svg+xml"
+                data={heart}
+              />
+            </button>
+            <span className="interactions__amount interactions__amount--heart">
+
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SocialCard;
--- social-card/src/index.js
+++ social-card/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import SocialCard from './SocialCard';
 import * as serviceWorker from './serviceWorker';
+const cards = document.querySelectorAll("[data-card='social-card']");
 
-ReactDOM.render(<App />, document.getElementById('root'));
+for(let i = 0; i < cards.length; i++) {
+  ReactDOM.render(
+    <SocialCard
+      imagePath={cards[i].getAttribute('data-image')}
+      title={cards[i].getAttribute('data-title')}
+    />,
+    cards[i]);
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
